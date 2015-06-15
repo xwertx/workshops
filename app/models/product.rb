1fe1d2f9a9bdd09cda6c8edec8d9bae5ad9e9b16
xwertx <rmadejowicz@tlen.pl>
2015-06-15 05:18:25 -0700

    fix product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,19 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :price
+
+  validates_format_of :price, :with => /\A\d+\.*\d{0,2}\z/ 
+
+  def average_rating
+    sum = 0.0
+    self.reviews.each do |r|
+      sum += r.rating
+    end
+    avg = sum / self.reviews.count
+  end
+
 end
