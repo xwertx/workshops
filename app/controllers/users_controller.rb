0589ff42f2ca1f766c51173b793287bfae0b606d
xwertx <rmadejowicz@tlen.pl>
2015-06-15 07:11:51 -0700

    add list 5 last user's reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
+  expose_decorated(:user) { current_user }
+  expose_decorated(:reviews, ancestor: :user)
+
   def show
+  	
   end
 
 end
