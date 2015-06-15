8de5c6d1edf48bf4f41a6b8a11c5946890244a80
xwertx <rmadejowicz@tlen.pl>
2015-06-15 05:34:07 -0700

    fix category and review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
+  
+  validates_presence_of :content
+  validates_presence_of :user_id
+  validates_presence_of :rating
+
   belongs_to :product
   belongs_to :user
 end
