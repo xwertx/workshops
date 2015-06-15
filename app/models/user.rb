8809dccbf3a4a787a1be1afad9db6de10f0522de
xwertx <rmadejowicz@tlen.pl>
2015-06-15 04:45:30 -0700

    add fields to users

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :firstname
+  validates_presence_of :lastname
+
+  has_many :reviews
+  has_many :products
+end
